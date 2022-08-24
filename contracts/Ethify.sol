7a7bfb1a71c2d2fe4f3143fe4adeda79582524c4
DhaivatV <71979720+DhaivatV@users.noreply.github.com>
2022-08-25 03:01:13 +0530

    contract updated

--- contracts/Ethify.sol
+++ contracts/Ethify.sol
@@ -17,9 +17,9 @@ contract Ethify{
 
     struct Inbox{
         uint numSentMessages;
-        uint numRecievedMessages;
+        uint numReceivedMessages;
         mapping(uint => Message) sentMessages;
-        mapping(uint => Message) recievedMessages;
+        mapping(uint => Message) receivedMessages;
     }
 
     mapping (address => Inbox) userInboxes;
@@ -35,8 +35,60 @@ contract Ethify{
          contractProperties.EthifyOwner = msg.sender;
     }
 
+    function checkUserRegistration() public view returns(bool){
+        return hasRegistered[msg.sender];
+    }
+
+    function clearInbox() public view {
+        Inbox storage newInbox =  userInboxes[msg.sender];
+    }
+
+    function registerUser() public {
+        if (!hasRegistered[msg.sender]){
+            Inbox storage newInbox = userInboxes[msg.sender];
+            hasRegistered[msg.sender] = true;
+            contractProperties.registeredUserAdderss.push(msg.sender);
+        }
+    }
+
+
     function getContractProperties() public view returns(address, address[] memory){
 
         return (contractProperties.EthifyOwner, contractProperties.registeredUserAdderss);
     }
+
+    function sendMessage(address _receiver, bytes32 _content) public {
+        newMessage.content = _content;
+        newMessage.timestamp = block.timestamp;
+        newMessage.sender = msg.sender;
+        // Update senders inbox
+        Inbox storage sendersInbox = userInboxes[msg.sender];
+        sendersInbox.sentMessages[sendersInbox.numSentMessages] = newMessage;
+        sendersInbox.numSentMessages++;
+
+        // Update receivers inbox
+        Inbox storage receiversInbox = userInboxes[_receiver];
+        receiversInbox.receivedMessages[receiversInbox.numReceivedMessages] = newMessage;
+        receiversInbox.numReceivedMessages++;
+        return;
+    }
+    
+    function receiveMessages() public view returns (bytes32[16] memory, uint[] memory, address[] memory) {
+        Inbox storage receiversInbox = userInboxes[msg.sender];
+        bytes32[16] memory content;
+        address[] memory sender = new address[](16);
+        uint[] memory timestamp = new uint[](16);
+        for (uint m = 0; m < 15; m++) {
+            Message memory message = receiversInbox.receivedMessages[m];
+            content[m] = message.content;
+            sender[m] = message.sender;
+            timestamp[m] = message.timestamp;
+        }
+        return (content, timestamp, sender);
+    }
+
+    function getMyInboxSize() public view returns (uint, uint) {
+        return (userInboxes[msg.sender].numSentMessages, userInboxes[msg.sender].numReceivedMessages);
+    }
+
 }
